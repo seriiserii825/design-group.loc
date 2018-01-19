43d905d21bc5e22b3bcef9d59d56c25119b3fe79
serii <seriiburduja@gmail.com>
2018-01-19 13:00:23 +0200

    finish our-services

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,29 +1,44 @@
-$(function () {
-  /*
-      https://www.jqueryscript.net/animation/Smooth-Mouse-Wheel-Scrolling-Plugin-With-jQuery-easeScroll.html
-      ===========================*/
-  $("html").easeScroll({
-    frameRate: 60,
-    animationTime: 1000,
-    stepSize: 90,
-    pulseAlgorithm: 1,
-    pulseScale: 8,
-    pulseNormalize: 1,
-    accelerationDelta: 20,
-    accelerationMax: 1,
-    keyboardSupport: true,
-    arrowScroll: 50,
-    touchpadSupport: true,
-    fixedBackground: true
-  });
+$(function(){
+    /*
+        https://www.jqueryscript.net/animation/Smooth-Mouse-Wheel-Scrolling-Plugin-With-jQuery-easeScroll.html
+        ===========================*/
+    $("html").easeScroll({
+        frameRate: 60,
+        animationTime: 1000,
+        stepSize: 90,
+        pulseAlgorithm: 1,
+        pulseScale: 8,
+        pulseNormalize: 1,
+        accelerationDelta: 20,
+        accelerationMax: 1,
+        keyboardSupport: true,
+        arrowScroll: 50,
+        touchpadSupport: true,
+        fixedBackground: true
+    });
 
-  /* show menu
-  ====================================================*/
-  $('#js-show-menu').on('click', function () {
-    $('#js-main-menu').slideToggle(1000);
-  });
+    /* show menu
+    ====================================================*/
+    $('#js-show-menu').on('click', function(){
+        $('#js-main-menu').slideToggle(1000);
+    });
+
+    /* slider
+    ====================================================*/
+    $('#js-slider').slick({
+        dots: true,
+        infinite: true,
+        speed: 1500,
+        autoplay: true,
+        autoplaySpeed: 2500,
+        fade: true,
+        cssEase: 'linear',
+        pauseOnHover: false
+    });
 
 });
+
+
 
 
 
